380157fc808a5da9532ffbeecb691c2ef46609af
rudy <doomdexter07@gmail.com>
2022-09-02 02:17:02 +0530

    added error page

--- controller/errorController.js
+++ controller/errorController.js
@@ -2,10 +2,16 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || `Error`;
 
-  res.status(err.statusCode).json({
-    status: err.statusCode,
-    message: err.message,
-    error: err,
-    stack: err.stack,
-  });
+  if (req.originalUrl.startsWith("/backpack")) {
+    res.status(err.statusCode).json({
+      status: err.statusCode,
+      message: err.message,
+      error: err,
+      stack: err.stack,
+    });
+  } else {
+    res.status(err.statusCode).render("error", {
+      title: `Something went Wrong`,
+    });
+  }
 };
